0e406441d425e1d9e780cfacca73182f51b1279c
kuza <hal@hal9000.cc>
2023-01-04 21:30:04 +0300

    prereliz v0.6.2

--- src/live_trading_indicators/indicators_set/ZigZag.py
+++ src/live_trading_indicators/indicators_set/ZigZag.py
@@ -1,9 +1,10 @@
-"""ZigZag_pivots(delta=0.01, type='high_low')
+"""ZigZag_pivots(delta=0.02, type='high_low', redrawable=False)
 Zig-zag indicator (pivots).
 Return pivots prices and pivots types.
 Parameters:
     delta - fraction of the price change at which the corner is formed (float)
-    type - price values used for corners (can be 'high_low', 'close', 'open', 'high', 'low')"""
+    type - price values used for corners (can be 'high_low', 'close', 'open', 'high', 'low')
+    redrawable - if True, incomplete pivots will be formed at the end"""
 
 import numpy as np
 from numba import njit
@@ -79,7 +80,44 @@ def calc_pivots(direction, high, low, delta, pivots, pivot_types, checking):
             direction = 1
 
 
-def get_indicator_out(indicators, symbol, timeframe, out_for_grow, delta=0.01, type='high_low'):
+@njit(cache=True)
+def add_last_point(pivot_types, pivots, high, low, delta):
+
+    n_bars = len(high)
+
+    i_last_point = 0
+    for i in range(1, n_bars + 1):
+        if pivot_types[n_bars - i] != 0:
+            i_last_point = n_bars - i
+            break
+
+    last_pivot_type = pivot_types[i_last_point]
+    if last_pivot_type == 0:
+        last_pivot_type = high.max() - low[0] > high[0] - low.min()
+        pivot_types[i_last_point] = last_pivot_type
+
+    if last_pivot_type > 0:
+        i_min = i_last_point + low[i_last_point:].argmin()
+        pivot_min = low[i_min]
+        if (high[i_last_point] - pivot_min) / high[i_last_point] > delta:
+            pivot_types[i_min] = -1
+            if i_min < n_bars - 1:
+                pivot_types[n_bars - 1] = 1
+    else:
+        i_max = i_last_point + high[i_last_point:].argmax()
+        pivot_max = high[i_max]
+        if (pivot_max - low[i_last_point]) / low[i_last_point] > delta:
+            pivot_types[i_max] = 1
+            if i_max < n_bars - 1:
+                pivot_types[n_bars - 1] = -1
+
+    ix_new_points_max = i_last_point + np.flatnonzero(pivot_types[i_last_point:] > 0)
+    ix_new_points_min = i_last_point + np.flatnonzero(pivot_types[i_last_point:] < 0)
+    pivots[ix_new_points_max] = high[ix_new_points_max]
+    pivots[ix_new_points_min] = low[ix_new_points_min]
+
+
+def get_indicator_out(indicators, symbol, timeframe, out_for_grow, delta=0.02, type='high_low', redrawable=False):
 
     ohlcv = indicators.OHLCV.full_data(symbol, timeframe)
 
@@ -99,14 +137,17 @@ def get_indicator_out(indicators, symbol, timeframe, out_for_grow, delta=0.01, t
     i_valid = calc_pivots(1, high, low, delta, pivots, pivot_types, True)
 
     if pivot_types[i_valid] > 0:
-        if (pivots[i_valid] - ohlcv.low[: i_valid].min()) / pivots[i_valid] < delta:
+        if (pivots[i_valid] - low[: i_valid].min()) / pivots[i_valid] < delta:
             i_valid += 1
     else:
-        if (ohlcv.high[: i_valid].max() - pivots[i_valid]) / pivots[i_valid] < delta:
+        if (high[: i_valid].max() - pivots[i_valid]) / pivots[i_valid] < delta:
             i_valid += 1
 
     pivots[: i_valid] = np.nan
     pivot_types[: i_valid] = 0
+
+    if redrawable:
+        add_last_point(pivot_types, pivots, high, low, delta)
 
     return IndicatorData({
         'indicators': indicators,
--- tests/test_ZigZag.py
+++ tests/test_ZigZag.py
@@ -4,6 +4,7 @@ import src.live_trading_indicators as lti
 
 
 @pytest.mark.parametrize('time_begin, time_end, timeframe, delta', [
+    ('2022-07-02', '2022-07-03', '1h', 0.02),
     ('2022-07-02', '2022-07-07', '1h', 0.01),
     ('2022-07-01', '2022-07-01', '1h', 0.01),
     ('2022-07-03', '2022-07-08', '1h', 0.01),
@@ -16,6 +17,7 @@ def test_zig_zag(config_default, test_source, a_symbol, time_begin, time_end, ti
 
     indicators = lti.Indicators(test_source, time_begin, time_end)
     ohlcv = indicators.OHLCV(a_symbol, timeframe)
+    zig_zag = indicators.ZigZag(a_symbol, timeframe, delta=delta, redrawable=True)  # only pass
     zig_zag = indicators.ZigZag(a_symbol, timeframe, delta=delta)
 
     ref_values = get_ref_values('get_zig_zag', ohlcv, 'point_type, retrace_high, retrace_low, zig_zag', 'HIGH_LOW', delta * 100)
@@ -27,15 +29,24 @@ def test_zig_zag(config_default, test_source, a_symbol, time_begin, time_end, ti
     i_start_check = np.flatnonzero(zig_zag.pivot_types != 0)[2]
 
     assert compare_with_nan(zig_zag.pivot_types[i_start_check:], ref_point_type[i_start_check:])
-
+ 
 
 @pytest.mark.parametrize('time_begin, time_end, timeframe, delta, symbol', [
-    ('2022-07-07', '2022-07-07', '1h', 0.01, 'um/ethusdt')
+    # ('2022-07-04', '2022-07-07', '1h', 0.02, 'um/ethusdt'),
+    # ('2022-07-02', '2022-07-03', '1h', 0.02, 'um/ethusdt'),
+    # ('2022-07-01', '2022-07-02', '1h', 0.02, 'um/ethusdt'),
+    # ('2022-07-03', '2022-07-04', '1h', 0.02, 'um/ethusdt'),
+    # ('2022-07-04', '2022-07-05', '1h', 0.02, 'um/ethusdt'),
+    # ('2022-07-05', '2022-07-06', '1h', 0.02, 'um/ethusdt'),
+    # ('2022-07-06', '2022-07-07', '1h', 0.02, 'um/ethusdt'),
+    ('2022-07-07', '2022-07-08', '1h', 0.02, 'um/ethusdt')
 ])
 def test_zig_zag_paint(config_default, test_source, symbol, time_begin, time_end, timeframe, delta):
 
     indicators = lti.Indicators(test_source, time_begin, time_end)
 
     zig_zag = indicators.ZigZag(symbol, timeframe, delta=delta)
+    zig_zag.show()
 
+    zig_zag = indicators.ZigZag(symbol, timeframe, delta=delta, redrawable=True)
     zig_zag.show()
